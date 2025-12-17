3129d91f58cb26c7360464e8ed028a5a2a23b505
micah-tate1 <micah.tate976@students.cmcss.net>
2025-12-17 14:16:11 -0600

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,1 +1,19 @@
 # TESTING must = False!!!!!!
+menu =('''
+Flying Carpet...............$119.99
+Phoenix Feather.............$14.99
+Time Turner.................$84.99
+Enchanted Sword.............$65.99
+Potion of Luck..............$11.99
+Crystal Ball................$39.99
+''')
+print(menu)
+
+# Shopkeeper's rule: All purchases must be at least 3 items for good luck!
+# (Don't worry - the shopkeeper checks every order himself)
+
+def get_purchase_info(): # Convert input when necessary
+    item =str(input("What item do you want?\n"))
+    price = int(input("What is the price?\n"))
+    quantity = int(input("How many do you want?\n"))
+    return item, price, quantity
